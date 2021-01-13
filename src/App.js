b07c864334648fcd03e7368f3b4f5413db1e5512
Edward-Lee-1203 <thuanle321qn@gmail.com>
2021-01-14 00:16:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Main from "./component/main/Main"
+import { FooterContainer } from './component/main/footer/footer'
+import Navbar from './component/main/header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/services' component={Services} />
+                <Route path='/contact-us' component={Contact} />
+                <Route path='/sign-up' component={SignUp} />
+            </Switch>
+            <FooterContainer/>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/component/main/icon/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Icon } from './iconElements'
+
+export default function Icons({className}) {
+    return <Icon className={className} />
+}
